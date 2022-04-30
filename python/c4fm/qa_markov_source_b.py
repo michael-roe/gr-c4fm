66ae23d7af276ce0e51697cdd3089e857a013a23
Michael Roe <michael-roe@users.noreply.github.com>
2022-04-30 19:04:21 +0100

    Added a test for markov_source_b.

--- python/c4fm/qa_markov_source_b.py
+++ python/c4fm/qa_markov_source_b.py
@@ -10,12 +10,14 @@ from gnuradio import gr, gr_unittest
 # from gnuradio import blocks
 try:
   from gnuradio.c4fm import markov_source_b
+  from gnuradio import blocks
 except ImportError:
     import os
     import sys
     dirname, filename = os.path.split(os.path.abspath(__file__))
     sys.path.append(os.path.join(dirname, "bindings"))
     from gnuradio.c4fm import markov_source_b
+    from gnuradio import blocks
 
 class qa_markov_source_b(gr_unittest.TestCase):
 
@@ -26,14 +28,27 @@ class qa_markov_source_b(gr_unittest.TestCase):
         self.tb = None
 
     def test_instance(self):
-        # FIXME: Test will fail until you pass sensible arguments to the constructor
-        instance = markov_source_b()
+        instance = markov_source_b(0.25, 1.0, 0)
 
     def test_001_descriptive_test_name(self):
-        # set up fg
-        self.tb.run()
+        rng = markov_source_b(0.25, 1.0, 0)
+        head = blocks.head(gr.sizeof_char, 2000)
+        convert = blocks.uchar_to_float()
+        integrate = blocks.integrate_ff(1000)
+        dst = blocks.vector_sink_f()
+        self.tb.connect(rng, head)
+        self.tb.connect(head, convert)
+        self.tb.connect(convert, integrate)
+        self.tb.connect(integrate, dst)
+        self.tb.run ()
         # check data
-
+        # The result is a random variable of mean 0.25*1000 = 250
+        # This test is statistical: there is a small probablity that it will
+        # fail.
+        self.assertLess(dst.data()[0], 350.0)
+        self.assertGreater(dst.data()[0], 150.0)
+        self.assertLess(dst.data()[1], 350.0)
+        self.assertGreater(dst.data()[1], 150.0)
 
 if __name__ == '__main__':
     gr_unittest.run(qa_markov_source_b)
